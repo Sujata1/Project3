35474abbc04a09bca4eddae1de4aed19e207e292
JeffONGPH <36478153+JeffONGPH@users.noreply.github.com>
2018-10-29 20:52:54 -0400

    Update rate.js

--- client/src/components/dashboard/rate.js
+++ client/src/components/dashboard/rate.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const RateButton = props => {
   return (
-    <button type="submit" class="btn btn-primary rate rightbutton rateButton" >Rate Me</button>
+
+<button type="button" class="btn btn-primary rate rightbutton rateButton" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Rate Me</button>
+
+
   )
 }
 
 export default RateButton;
-
 
