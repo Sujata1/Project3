c4da2eb1daa1569a1ad1d4d5dcdb3bf9830e3dd0
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-31 14:21:31 -0400

    background image & styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = props => {
   return (
     <header>
+      <div id='headerText'>PRESS THE FREEDOM</div>
       {props.isLoggedIn && 
         <div>
           {props.currentUser && 
