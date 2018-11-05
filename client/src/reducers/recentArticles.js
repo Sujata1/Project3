cd5c1beace09fe85d08dddedf29bc406562e6fb8
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-11-05 14:51:35 -0500

    fix duplicate recent articles

--- client/src/reducers/recentArticles.js
+++ client/src/reducers/recentArticles.js
@@ -3,6 +3,9 @@ import {RECENTARTICLES_ADD, RECENTARTICLES_RESET} from '../actions/addRecentArti
 const recentArticles = (state = [], action) => {
   switch(action.type) {
     case RECENTARTICLES_ADD:
+      for(let i=0; i<state.length; i++) {
+        if(state[i].url === action.article.url) return state;
+      }
       return [
         action.article,
         ...state
